035c9407a055b9271bbd42462fb8ad289a40b711
김희경 <hgkim@rianag.com>
2023-06-08 10:22:42 +0900

    webpack-vue3-eslint-origin

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1 @click="increase
+  ">{{ count }}
+  </h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <hellow 
+    v-for="fruit in fruits" 
+    :key="fruit" 
+    :name="fruit">
+    {{ fruit }}
+    </hellow>
+  </ul>
+</template>
+
+<script>
+import Fruit from '~/components/Fruit'
+export default{
+  components:{
+    hellow: Fruit
+  },
+  data(){
+    return {
+      count: 0,
+      fruits: ['apple', 'banana', 'cherry']
+    }
+  },
+  methods: {//메소드 > 이벤트 핸들러 /반응성 (데이터를 갱신하면 화면이 바뀐다.)
+    increase(){
+      this.count += 1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+h1{
+  font-size: 50px;
+  color: royalblue;
+}
+ul{
+  li{
+    font-size: 40px;
+  }
+}
+</style>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>{{ name }}</li>
+
+</template>
+
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"> //scoped는 현재페이지에서만 스타일이 적용되게끔 유효범위를 설정함
+h1{
+  color: red !important;
+}
+</style>
